97f40f0866c01882f034a7393154a198893fef0f
Karla Susana Mendoza Romero <A01566555@itesm.mx>
2019-05-02 13:35:42 -0600

    Initial Commit

---
+++ Test3.2/Test3.2/Generic/Generico.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test3._2.Generic
+{
+    class Generico <Arreglo>
+    {
+        public static void insertionSort(int[] arr, int n)
+        {
+            int i, key, j;
+            for (i = 1; i < n; i++)
+            {
+                key = arr[i];
+                j = i - 1;
+
+                /* Move elements of arr[0..i-1], that are
+                   greater than key, to one position ahead
+                   of their current position */
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+        }
+    }
+}
